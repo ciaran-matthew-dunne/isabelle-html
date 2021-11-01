1182afd7203117670e1bdb2fa00793592a657f64
ciaran <cmd1@hw.ac.uk>
2021-11-01 21:13:25 +0000

    lots of work on parsing.py

--- scripts/parsing.py
+++ scripts/parsing.py
@@ -2,19 +2,20 @@
 # ∙ Add support for definitions where the fact different to the name
 #   given after the 'where' keyword
 # ∙ Add support for definition names in quotes
+from lxml import etree as etree
+import os
+import subprocess
 
-import xml.etree.ElementTree as etree
-
-### Preparing stuff
+## DEBUGGING STUFF:
 gzf_path = "../out/html/Unsorted/GZF-HOL/Model_locale.html"
 gzf_tree = etree.parse(gzf_path)
 gzf_root = gzf_tree.getroot()
 spans = gzf_root[1][1]
-
+theories = ['ML_Bootstrap', 'Pure', 'Sessions', 'Code_Generator', 'ATP', 'Archimedean_Field', 'Argo', 'BNF_Cardinal_Arithmetic', 'BNF_Cardinal_Order_Relation', 'BNF_Composition', 'BNF_Def', 'BNF_Fixpoint_Base', 'BNF_Greatest_Fixpoint', 'BNF_Least_Fixpoint', 'BNF_Wellorder_Constructions', 'BNF_Wellorder_Embedding', 'BNF_Wellorder_Relation', 'Basic_BNF_LFPs', 'Basic_BNFs', 'Binomial', 'Code_Evaluation', 'Code_Numeral', 'Complete_Lattices', 'Complete_Partial_Order', 'Complex', 'Complex_Main', 'Conditionally_Complete_Lattices', 'Ctr_Sugar', 'Deriv', 'Divides', 'Enum', 'Equiv_Relations', 'Euclidean_Division', 'Extraction', 'Factorial', 'Fields', 'Filter', 'Finite_Set', 'Fun', 'Fun_Def', 'Fun_Def_Base', 'GCD', 'Groebner_Basis', 'Groups', 'Groups_Big', 'Groups_List', 'HOL', 'Hilbert_Choice', 'Hull', 'Inductive', 'Inequalities', 'Int', 'Lattices', 'Lattices_Big', 'Lazy_Sequence', 'Lifting', 'Lifting_Set', 'Limited_Sequence', 'Limits', 'List', 'MacLaurin', 'Main', 'Map', 'Meson', 'Metis', 'Modules', 'Nat', 'Nitpick', 'NthRoot', 'Num', 'Numeral_Simprocs', 'Nunchaku', 'Option', 'Order_Relation', 'Orderings', 'Parity', 'Partial_Function', 'Power', 'Predicate', 'Predicate_Compile', 'Presburger', 'Product_Type', 'Quickcheck_Exhaustive', 'Quickcheck_Narrowing', 'Quickcheck_Random', 'Quotient', 'Random', 'Random_Pred', 'Random_Sequence', 'Rat', 'Real', 'Real_Vector_Spaces', 'Record', 'Relation', 'Rings', 'SAT', 'SMT', 'Semiring_Normalization', 'Series', 'Set', 'Set_Interval', 'Sledgehammer', 'String', 'Sum_Type', 'Topological_Spaces', 'Transcendental', 'Transfer', 'Transitive_Closure', 'Typedef', 'Typerep', 'Vector_Spaces', 'Wellfounded', 'Wfrec', 'Zorn', 'Code_Generator', 'Connection_locale', 'GZF_locale', 'Model_locale', 'Ord_locale']
 
 tmapI = [(spans[n],n) for n in range(180,186)]
 disjI = [(spans[n],n) for n in range(344,359)]
-tier_set = [(spans[n],n) for n in range(897,920)]
+tier_set = [(spans[n],n) for n in range(911,930)]
 
 def printe(elem):
   if elem.text:
@@ -36,6 +37,47 @@ def printspl(l):
       print("#" + str(n))
       printe(elem)             
 
+
+### Preparing stuff
+parser = etree.XMLParser(encoding="utf-8")
+chapter = 'Unsorted'
+session = 'GZF-HOL'
+root_dir = "../tsrc/"
+html_path = os.path.abspath('../out/html/')
+out_path  = os.path.abspath("../out/test/")
+root_path = os.path.abspath(root_dir)
+
+## Building HTML of session and dependencies
+def build_html():
+  sessions_cmd = "isabelle sessions -D " + root_path
+  tmp = b'Pure\nTools\nHOL\nGZF-HOL\n'
+  session_stdout = tmp.decode("utf-8")
+  # subprocess.run(sessions_cmd, shell=True, capture_output=True).stdout.decode("utf8")
+  all_sessions = ' '.join(list(filter(None, session_stdout.split('\n'))))
+  option = '-o quick_and_dirty'
+  build_cmd = "isabelle env\
+    ISABELLE_BROWSER_INFO=" + html_path +\
+    " isabelle build " + option + " -d " + root_path +\
+    " -c -o browser_info " + all_sessions
+
+## Getting names of theories in session dependencies
+def get_thys(session):
+  files_cmd = "isabelle build -n -l -d " + root_path + " " + session
+  files_stdout = subprocess.run(files_cmd, shell=True, capture_output=True)\
+                           .stdout.decode("utf8").split('\n')
+  return [ line.rsplit(os.sep, 1)[1].rsplit('.',1)[0] for line in files_stdout\
+            if line[-4:] == '.thy']
+
+def get_html_paths(thys):
+  html_paths = []
+  for subdir, dirs, files in os.walk(html_path):
+    for filename in files:
+      if filename[-5:] == '.html'\
+      and filename[-8:-5] != '.ML'\
+      and filename[:-5] in thys:
+        html_paths.append(subdir + os.sep + filename)
+  return html_paths
+
 ### Parsing names of declarations
 def is_decl(elem, decl_name):
   items = list(elem.itertext())
@@ -44,48 +86,39 @@ def is_decl(elem, decl_name):
 def decl_inds(spans, decl_name):  
   return [ n for n in range(len(spans)) if is_decl(spans[n], decl_name)]
 
-# Traverses tree and creates hooks for each definition,
-# returning a list of each definition name. 
-def get_def_names(spans, thy):
-  def_inds = decl_inds(spans, "definition")
-  def_names = []
-  for n in def_inds:
-    elem = spans[n+1]
-    name = elem.text
-    if name not in [None, "\""]:
-      def_names.append(name + "_def")
-      elem.set('id', thy + '.' + name + "_def")
-  return def_names
+# Traverses tree and creates hooks for each theorem/definition,
+# returning a list of each definition name.
+def hook_and_get_thm_names(spans):
+  n = 0; thm_names = []
+  while n in range(len(spans)):
+    if spans[n][0:]:
+      if spans[n][0].text in ['lemma','theorem','corollary',\
+                             'proposition','lemmas','named_theorems']:
+        elem = spans[n]
+        name = elem.tail.strip()
+        thm_names.append(name)
+        elem.set('id',  name)
+      elif spans[n][0].text == 'definition':
+        elem = spans[n+1]
+        name = elem.text
+        if name not in [None, "\""]:
+          thm_names.append(name + "_def")
+          elem.set('id', name + "_def")      
+    n += 1                             
+  return list(filter(None,thm_names))
 
-def get_thm_names(spans,thy):
-  thm_inds = decl_inds(spans, "lemma") \
-          + decl_inds(spans, "theorem") \
-          + decl_inds(spans, "corollary") \
-          + decl_inds(spans, "proposition") \
-          + decl_inds(spans, "lemmas") \
-          + decl_inds(spans, "named_theorems")
-  thm_names = []
-  for n in thm_inds:
-    elem = spans[n]
-    name = elem.tail.strip()
-    thm_names.append(name)
-    elem.set('id', thy + '.' + name)
-  return thm_names
+def hook_and_gen_library(html_files):
+  fact_library = []
+  for file in html_files:
+    print(file)
+    html_tree = etree.parse(file,parser=etree.XMLParser(encoding='utf-8',recover=True))
+    html_root = html_tree.getroot()
+    spans = html_root[1][1]
+    thms = hook_and_get_thm_names(spans)
+    fact_library.append((file,html_tree,thms))
+  return fact_library  
 
 #### Inserting links to facts
-def fact_link(name, thy, path):
-  elem = etree.Element('html:a')
-  elem.set('class','pst-link')
-  elem.set('href', path + "#" + thy + "." + name)
-  elem.text = name
-  return elem
-
-def text_elem(name):
-  elem = etree.Element('html:a')
-  elem.set('class','unlinked-fact')
-  elem.text = name
-  return elem
-
 def fact_split(str):
   if str == '': 
     return []
@@ -100,7 +133,20 @@ def fact_split(str):
     while str[i:] and str[i] not in ['\n', ' ']:
       hd += str[i]
       i += 1
-    return [hd] + fact_split(str[i:]) 
+    return [hd] + fact_split(str[i:])
+
+def fact_link(name, thy, path):
+  elem = etree.Element('html:a')
+  elem.set('class','pst-link')
+  elem.set('href', path + "#" + thy + "." + name)
+  elem.text = name
+  return elem
+
+def text_elem(name):
+  elem = etree.Element('html:a')
+  elem.set('class','pad')
+  elem.text = name
+  return elem
 
 def add_elem_links(elem, fact_bank, thy, path):
   items = fact_split(elem.tail)
@@ -109,39 +155,38 @@ def add_elem_links(elem, fact_bank, thy, path):
       if txt in fact_bank else text_elem(txt)\
       for txt in items])
 
-def insert_keyword_thm_links(spans, fact_bank, thy, path):
-  keyword_inds = decl_inds(spans, "from")\
-             + decl_inds(spans, "with")\
-             + decl_inds(spans, "using")\
-             + decl_inds(spans, "unfolding")
-  for n in keyword_inds:
-    m = n
-    print("n=" + str(n))
-    while (spans[m:]) and (m == n or spans[m].get("class") not in ['keyword1', 'keyword3']):
-      tl = spans[m].tail
-      if tl and tl.strip() != '':
-        add_elem_links(spans[m], fact_bank, thy, path)
-      m += 1  
-    print("m="+ str(m))  
+## TODO: link facts invoked by 'note' and 'thm' and 'declare'
+def thm_links(spans, fact_bank, thy, path):
+  i = 0 
+  while i in range(len(spans)):
+    ## Keyword case:
+    if spans[i][0:] and spans[i][0].text in ['from','with','using','unfolding']:
+      j = i
+      while (spans[j:]) and (i == j or\
+        not (spans[j].get("class") in ['keyword1', 'keyword3'])):
+        tl = spans[j].tail
+        if tl and tl.strip() != '':
+          add_elem_links(spans[j], fact_bank, thy, path)
+        j += 1  
+    ## Quasi-keyword case
+    if spans[i].text in ['rule','use','unfold','fact']\
+    or spans[i].get("class") == 'quasi_keyword':
+      j = i
+      while (spans[j:]) and (i == j or\
+        not (spans[j].text == ')'\
+         or spans[j].get("class") in ['quasi_keyword','keyword2']\
+         or (spans[j][0:] and spans[j][0].text == ','))):
+        tl = spans[j].tail
+        if tl and tl.strip() != '':
+          add_elem_links(spans[j], fact_bank, thy, path)
+        j += 1  
+    i += 1     
 
-
-# After which keywords can facts be mentioned?
-# - using, from, with, unfolding
-# - note, thm
-# - simp add:|only:, auto|blast intro:|elim:, 
-# - unfold, rule, induct, subst, insert, cases, OF, THEN
-
-def link_html():
-  facts = get_thm_names(spans, "Model_locale") + get_def_names(spans, "Model_locale")
-  insert_keyword_thm_links(spans, facts, "Model_locale", gzf_path)
-  f = open("../out/test/Model.xml",'wb')
+def link_html(path, out_dir):
+  thy = path.split('/')[-1].split('.')[0]
+  out_file = out_dir + "/" + thy + ".xml"
+  facts = get_thm_names(spans, thy) + get_def_names(spans, thy)
+  thm_links(spans, facts, thy, out_file)
+  f = open(out_file, 'wb')
   gzf_tree.write(f, method='html')
   f.close()
-# When elem.text == lemma, let lem_name := elem.tail
-# We add the extra key 'id' with value 'GZF_locale.GBL.lem_name' 
-
-# Occurences of facts need to be replaced with  
-# <a class="pst-link" 
-#    href="http://www.macs.hw.ac.uk/~cmd1/IsabelleGZF/GZF_locale.html#GZF_locale.GBL.fact_name">
-#  fact_name  
-# </a>
